b32ea524130cd26bdc0fa88041b0096d9d9a7ba9
Meshach Manzano <meshachmanzano@gmail.com>
2022-08-10 19:59:39 +0100

    final form

--- js/main.js
+++ js/main.js
@@ -5,5 +5,8 @@ for (i = 0; i < faq.length; i++) {
         this.classList.toggle("active");
         var body = this.nextElementSibling;
         body.classList.toggle("faq-body-show")
+        if (body.classList.contains("faq-body-show"))
+            body.classList.remove("faq-body-hide")
+        else body.classList.add("faq-body-hide")
     });
 }
